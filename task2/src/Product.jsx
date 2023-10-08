afbbb546269e1764e81378aa7294d41bb63adf12
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-10-08 15:57:00 +0300

    React Router

--- task2/src/Product.jsx
+++ task2/src/Product.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
 
-function Product() {
-  const { productId } = useParams();
-  return <div className="product"> {productId}</div>;
+function Product({ match }) {
+  return <div className="product">{match.params.productId}</div>;
 }
 
 export default Product;
